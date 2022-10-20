17c8c69a73fddae3e15e280963b51c1b331b49de
ramonS2999 <ramonlima3897@gmail.com>
2022-10-20 15:32:13 -0300

    Finalizando os movimentos dos alienígenas

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -6,6 +6,7 @@ from alien import Alien
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Responde a pressionamento de tecla."""
+
     if event.key == pygame.K_RIGHT:
         # Move a espaçonave para a direita
         ship.moving_right = True
@@ -20,6 +21,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_q:
         sys.exit()
 
+
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Dispara um projétio se o limite ainda não foi alcançado."""
     
@@ -28,7 +30,9 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
+
 def check_keyup_events(event, ship):
+
     """Responde a solturas de tecla."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -36,8 +40,10 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
+
 def cheeck_events(ai_settings, screen, ship, bullets):
     """Responde a eventos de pressionamento de teclas e de mouse."""
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -47,6 +53,7 @@ def cheeck_events(ai_settings, screen, ship, bullets):
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Atualiza as imagens na tela e altera para a nova tela."""
@@ -61,6 +68,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
     # Deixa a tela mais recente visível
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     """Atualiza a posição dos projéteis e se livra dos projéteis antigos."""
@@ -85,13 +93,15 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
-    
+
+
 def get_number_aliens_x(ai_settings, alien_width):
     """Determina o número de alienígenas que cabem em uma linha."""
-
+    
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
+
 
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
 
@@ -103,9 +113,35 @@ def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
+
 def get_number_rows(ai_settings, ship_height, alien_height):
     """Determina o número de linha com alienígenas que cabem na tela."""
 
     available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """Responde apropriadamente se algum alienígena alcançou uma borda."""
+
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Faz toda a frota descer e muda a sua direção."""
+
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    """ Verifica se a frota está em uma das bordas e então atualiza as posições de todos os alienígenas da frota."""
+
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -27,3 +27,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = PINK
         self.bullets_allowerd = 3
+
+        # Configurações dos alienígenas
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda 
+        self.fleet_direction = 1
